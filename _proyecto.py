a1315c1026416257e0a6cd56d459e0dc0df3d3c7
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2023-03-30 14:59:17 -0500

    Actividad finalizada

--- _proyecto.py
+++ _proyecto.py
@@ -10,3 +10,24 @@ computer_option = 'piedra'
 
 if user_option == computer_option:
     print('Empate!')
+elif user_option == 'piedra':
+    if computer_option == 'tijera':
+        print('Piedra gana a tijera')
+        print('User gano¡')
+    else:
+        print('Papel gana a piedra')
+        print('Computer gano¡')
+elif user_option == 'papel':
+    if computer_option == 'piedra':
+        print('Papel gana a piedra')
+        print('User gano¡')
+    else:
+        print('Tijera gana a papel')
+        print('Computador gano¡')
+elif user_option == 'tijeras':
+    if computer_option == 'papel':
+        print('Tijera gana a papel')
+        print('User gano¡')
+    else: 
+        print('Piedra gana a tijera')
+        print('Computer gano¡')
