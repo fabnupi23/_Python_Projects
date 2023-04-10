5f682d73e374675a77ea4d5766e6c9771bbb5210
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2023-04-10 11:44:51 -0500

    Aplicando tuplas al juego piedra, papel o tijera

--- _proyecto.py
+++ _proyecto.py
@@ -2,11 +2,25 @@
 crearemos un programa que ejecute la logica del juego piedra papel o tijera y asi jugar contra
 la computadora, poco a poco se ira incrementando el nivel de este proyecto 
 '''
+import random #importamos esta liberia, este módulo implementa generadores de números pseudoaleatorios para varias distribuciones.
+
+#Vamos a crear una tupla para estas opciones
+options = ('piedra', 'papel', 'tijera')
+
 
 #Primero creamos dos variables 
 
-user_option = input('piedra, papel o tijera => ')
-computer_option = 'piedra' 
+user_option = input('piedra, papel o tijera => ')   #Leemos la opción del usuario
+user_option = user_option.lower() #Pasamos todo a minuscula
+
+if not user_option in options:
+    print('Esa opción no es valida')
+
+
+computer_option = random.choice(options)  #La computadora tiene la opción piedra para siempre, con la implementación de Random se generara una opción aleatoria 
+
+print('User Options => ', user_option)
+print('Computer Options => ', computer_option)
 
 if user_option == computer_option:
     print('Empate!')
